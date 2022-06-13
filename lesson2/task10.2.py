e5ee21f3007f5980a63aa15470c4efcb2fbdfabf
Palina Chubina <palina1967038@gmail.com>
2022-06-13 12:15:46 +0300

    homework_2

--- lesson2/task10.2.py
+++ lesson2/task10.2.py
@@ -5,9 +5,10 @@ if (a + b > c) and (a + c > b) and (b + c > a):
     print('Треугольник существует.')
 else:
     print('Треугольник не существует.')
+
 if a != b and a != c and b != c:
     print('Треугольник разносторонний.')
 elif (a ==b or a == c) and (b == a or b == c) and (c == a or c == b):
     print('Треугольник равнобедренный.')
-elif a == b and a == c:
+elif a == b or a == c:
     print('Треугольник равносторонний.')
